ce49809f74710d3e475edce39cc72201590df6f9
Nick Gray <nick.gray@rea-group.com>
2019-06-12 09:01:25 +1000

    add dare logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import logo from '../images/dare-logo.png'
+import icon from '../images/dare-house.png'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            {/*<span className="icon fa-diamond"></span>*/}
+          <img src={icon} alt="dare property logo"/>
         </div>
         <div className="content">
             <div className="inner">
