995a58b6ff744b2daff80f0059fc4ded9052327b
Nick Gray <nicholasnbg@gmail.com>
2019-07-01 21:12:23 +1000

    Neaten up text stuff

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -5,10 +5,10 @@ const paragraphs = [
   `Country Victoria was our home until our early 20’s, all our extended family
           remain in the area -Melbourne is our home now with our children and
           grandchildren we have a well-worn track down the Hume Highway and
-          through picturesque Yea and will continue to do so happily for years to come.
-          We have always invested in residential property with just the last few years
-          seeing the area we grew up in with a different perspective.
-          Cash flow property is our investment vehicle and once we worked out a
+          through picturesque Yea and will continue to do so happily for years to come.`,
+          `We have always invested in residential property with just the last few years
+          seeing the area we grew up in with a different perspective.`,
+          `Cash flow property is our investment vehicle and once we worked out a
           profitable system that works for us, we would not invest any other way.
           Many years ago we had invested in the same area with ordinary results and
           were therefore not seeing the opportunity right in front of us due to this past
--- src/components/content/Results.js
+++ src/components/content/Results.js
@@ -1,39 +1,43 @@
 import React from 'react'
 import Content from '../Content'
 
-const paragraphs = [
-  `We purchase residential property in regional Victoria and
-manufacture growth to achieve gross returns of 8/9 % .
-The examples below are properties that needed work, the numbers
-worked and with a cosmetic tidy up adding perceived value and
-increasing the rent by $30/50 a week ensuring low maintenance in
-demand investment returning a minimum gross return of 8% .`,
-`*3 x 1 residential property purchase price $146,000, rental after
-spending $3,200 to tidy up $240 per week.`,
-`*3 x 1 residential property purchase price $140,000, very bad
-condition spent $11,500 and achieved a rental return of $290 per
-week`,
-`*3 x 1 residential property purchase price $156,000, spent $10,500
-and achieved a rental return of $295 per week.`,
-`*2 x 1 unit, purchase price $100,000, rental return after spending
-$11,500 was $220 per week.`,
-`*current project, purchase price $100,000 on a residential block of
+const Results = (props) => {
+  return (
+    <article
+      id={props.articleId}
+      className={`${props.active &&'active'} ${props.articleTimeout && 'timeout'}`}
+      style={{display: 'none'}}
+    >
+      <h2 className={"major"}>{props.title}</h2>
+      <p>We purchase residential property in regional Victoria and
+      manufacture growth to achieve gross returns of 8/9 % .</p>
+      <p>The examples below are properties that needed work, the numbers
+      worked and with a cosmetic tidy up adding perceived value and
+      increasing the rent by $30/50 a week ensuring low maintenance in
+      demand investment returning a minimum gross return of 8% .</p>
+      <ul>
+        <li>3 x 1 residential property purchase price $146,000, rental after
+        spending $3,200 to tidy up $240 per week.</li>
+        <li>3 x 1 residential property purchase price $140,000, very bad
+        condition spent $11,500 and achieved a rental return of $290 per
+        week.</li>
+        <li>3 x 1 residential property purchase price $156,000, spent $10,500
+        and achieved a rental return of $295 per week.</li>
+        <li>2 x 1 unit, purchase price $100,000, rental return after spending
+$11,500 was $220 per week.</li>
+        <li>current project, purchase price $100,000 on a residential block of
 approx. 800 m, a 3 x 1 house with a renovation spend of $15,000, will
-return a weekly rent of $290 minimum.`,
-`The properties above were good deals without any renovations they
+return a weekly rent of $290 minimum.</li>
+      </ul>
+      <p>The properties above were good deals without any renovations they
 could have been settled and rented easily without any work-we
-renovate to keep our options open to on sell when we choose.`
-]
-
-const Results = (props) =>
-  <Content
-    articleId={"results"}
-    title={"Results"}
-    paragraphs={paragraphs}
-    active={props.active}
-    articleTimeout={props.articleTimeout}
-    onCloseArticle={props.onCloseArticle}
-  />
+renovate to keep our options open to on sell when we choose.</p>
+      <div className="close" onClick={() => {
+        props.onCloseArticle()
+      }} />
+    </article>
+  )
+}
 
   export default Results
 
