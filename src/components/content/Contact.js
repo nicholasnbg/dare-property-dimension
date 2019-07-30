b6f05694dd1262f361da94c55dde08c000cf3705
Nick Gray <nicholasnbg@gmail.com>
2019-07-30 22:14:28 +1000

    minor changes

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -25,12 +25,12 @@ const Contact = props => {
           <li><input type="reset" value="Reset"/></li>
         </ul>
       </form>
-      <ul className="icons">
+      {/* <ul className="icons">
         <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
         <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
         <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
         <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
-      </ul>
+      </ul> */}
       <div className="close" onClick={() => {
         props.onCloseArticle()
       }}></div>
